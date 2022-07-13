7b702bbf2bf61d03134d9dc0bb234bcad9e263dc
Martin Gerdin Warnberg <martingerdin@gmail.com>
2022-07-13 21:26:07 +0200

    Add contentful

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 
 const fetch = require("node-fetch");
 const fs = require("fs");
+const contentful = require("contentful-management");
 require("dotenv").config();
 
 // fetch('https://api.github.com/users/github').then(response => response.json()).then(data => console.log(data));
 
 const koboApiKey = process.env.KOBO_API_KEY;
 const contentfulApiKey = process.env.CONTENTFUL_API_KEY;
+const contentfulSpace = process.env.CONTENTFUL_SPACE;
+
+const client = contentful.createClient({
+  accessToken: contentfulApiKey
+})
+
 
 fs.readFile("./response.json", "utf8", (err, response) => {
   const data = JSON.parse(response);
@@ -15,6 +22,22 @@ fs.readFile("./response.json", "utf8", (err, response) => {
   const submissionCounts = results.map(result => result.deployment__submission_count);
   console.log(submissionCounts);
 });
+
+client.getSpace(contentfulSpace)
+.then((space) => space.getEnvironment("master"))
+.then((environment) => environment.createEntry("enrolledPatients", {
+  fields: {
+    title: {
+      "en-US": "Enrolled patients",
+    },
+    numberOfEnrolledPatients: {
+      "en-US": {"test": "test"},
+    }
+  }
+}))
+.then((entry) => console.log(entry))
+.catch(console.error)
+
 
   
 
